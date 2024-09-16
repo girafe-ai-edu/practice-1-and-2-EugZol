acf43f00b13bf3a02dffd503127e3fb41300bd24
Eugene Zolotarev <eugzol@gmail.com>
2024-09-16 16:33:50 +0300

    Practice 1 and Practice 2 solutions

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
-BMI = weight/height**2 
+BMI = weight/height**2
 """
-weight = input()
-height = input()
+weight = input("Weight in kg: ")
+height = input("Height in cm: ") # Let's assume it is in cantimeters
 
+# code here
 
-#code here
+weight = float(weight)
+height = float(height) / 100
 
+if height == 0:
+    print("Height can't be zero")
+    exit(1)
+
+bmi = weight / (height)**2
+print(round(bmi, 2))
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,18 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+
+number = input("Enter four-digit number: ")
+
+try:
+    number = int(number)
+except ValueError:
+    print("That wasn't a number")
+    exit(1)
+
+if not (1000 <= number <= 9999):
+    print("That wasn't a four-digit number")
+    exit(1)
+
+result = sum(number // 10**i % 10 for i in range(0, 4))
+print(result)
